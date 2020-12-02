8107f8a8a135b40cdb721cc5375ac7ddf3e7cbc6
Stefano Zaghi <stefano.zaghi@gmail.com>
2020-12-02 18:38:20 +0100

    fix ghost mpi missing in adam, now seems to work again

--- src/lib/adam_adam_object.F90
+++ src/lib/adam_adam_object.F90
@@ -378,6 +378,7 @@ contains
    if (self%tree%nodes_number > 1) then
       self%field%u_s(:,:,:,1:self%field%blocks_number,1) = self%field%u(:,:,:,1:self%field%blocks_number)
       call self%field%update_ghost(s=1)
+      call self%field%update_ghost_mpi(s=1)
       self%field%u(:,:,:,1:self%field%blocks_number) = self%field%u_s(:,:,:,1:self%field%blocks_number,1)
    endif
    endsubroutine update_ghost
