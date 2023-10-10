1d0c8032d8d5105ff338120d9f55d7917ad80fa9
Stefano Zaghi <stefano.zaghi@gmail.com>
2023-10-10 08:43:05 +0200

    clean cristiano's mhd from nasto branch

--- src/app/mhd/common/adam_mhd_parameters.F90
+++
@@ -1,9 +0,0 @@
-!< ADAM, Navier-Stokes equations system parameters.
-module adam_mhd_parameters
-!< ADAM, Navier-Stokes equations system parameters.
-
-use PENF, only : I4P
-
-implicit none
-private
-endmodule adam_mhd_parameters
