ae0d9acfe45c157d15caef1abefd6d71ff21cee9
Giacomo Rossi <giacombum@gmail.com>
2023-10-17 01:38:39 -0700

    Add NASTO OpenMP offload test

---
+++ src/tests/nasto/gmp/shock-sphere/run_test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [ $# -eq 0 ] ; then
+   mpirun -n 1 adam_nasto_gmp adam-nasto-shock-sphere.ini | tee run_test.log
+else
+   mpirun -n 1 compute-sanitizer adam_nasto_gmp adam-nasto-shock-sphere.ini | tee run_test.log
+fi
