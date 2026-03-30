3055e21f344bb3263ed3ec093400e45d9e403cc2
Stefano Zaghi <stefano.zaghi@gmail.com>
2026-03-30 19:41:29 +0200

    refactor(prism): consolidate program-scope singletons in common and FNL libraries
    - move adam_prism_globals to common library
    - add adam_prism_fnl_globals to FNL library
    - reorganize use statements to reflect singleton pattern
    - update module comments to clarify scope and purpose

--- src/app/prism/common/adam_prism_common_library.F90
+++ src/app/prism/common/adam_prism_common_library.F90
@@ -2,13 +2,12 @@
 module adam_prism_common_library
 !< ADAM PRISM common library, entry for all common classes and libraries.
 
-! PRISM modules
+! PRISM common classes, libraries, parameters
 use :: adam_prism_bc_object
 use :: adam_prism_coil_object
 use :: adam_prism_common_object
 use :: adam_prism_external_fields_object
 use :: adam_prism_fWLayer_object
-use :: adam_prism_globals
 use :: adam_prism_ic_object
 use :: adam_prism_io_object
 use :: adam_prism_leapfrog_pic_object
@@ -21,6 +20,8 @@ use :: adam_prism_rk_bc_object
 use :: adam_prism_rk_pic_object
 use :: adam_prism_riemann_library
 use :: adam_prism_time_object
+! PRISM singleton objects
+use :: adam_prism_globals
 
 implicit none
 public
--- src/app/prism/fnl/adam_prism_fnl_library.F90
+++ src/app/prism/fnl/adam_prism_fnl_library.F90
@@ -2,15 +2,16 @@
 module adam_prism_fnl_library
 !< ADAM PRISM FNL library, entry for all PRISM FNL (and common) classes and libraries.
 
-! PRISM modules
+! ADAM FNL classes, libraries, parameters
+use :: adam_fnl_library
+! PRISM common classes, libraries, parameters
 use :: adam_prism_common_library
+! PRISM FNL classes, libraries, parameters
 use :: adam_prism_fnl_coil_object
-use :: adam_prism_fnl_coil_global
 use :: adam_prism_fnl_external_fields_kernels
 use :: adam_prism_fnl_fwlayer_object
-use :: adam_prism_fnl_fwlayer_global
-! ADAM modules
-use :: adam_fnl_library
+! PRISM FNL singleton objects
+use :: adam_prism_fnl_globals
 
 implicit none
 public
